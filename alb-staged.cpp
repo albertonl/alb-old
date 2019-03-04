fe4c5b542b8f3fceba1cdcb67b58154097c563e1
Alberto Navalon <alberto.navalonlillo@gmail.com>
2019-03-04 10:44:58 +0100

    Update staged version of ALB interpreter

--- alb-staged.cpp
+++ alb-staged.cpp
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+const static std::vector<string> level_plus = {"BEGIN","VAR","repeat","if","else","elif"};
+const static std::vector<string> level_minus = {"END","ENDVAR","ENDLOOP","COND_END"};
+
 struct Statement{
 		string statement;
 		int level;
@@ -48,6 +51,11 @@ class Program{
 void Program::read(string fileName){
 	ifstream source;
 	string actualLine;
+	std::vector<string> tmp_program;
+
+	int level=0;
+	std::vector<int> index_on_level(1,0);
+	bool gotCoincidence=false;
 
 	source.open(fileName.c_str(), ios::in);
 
@@ -58,11 +66,11 @@ void Program::read(string fileName){
 				stringstream ss(actualLine);
 				if(actualLine.find(" ")!=string::npos){
 					while(getline(ss,actualLine,' ')){
-						program.push_back(actualLine);
+						tmp_program.push_back(actualLine);
 					}
 				}
 				else{
-					program.push_back(actualLine);
+					tmp_program.push_back(actualLine);
 				}
 			}
 		}
@@ -75,6 +83,34 @@ void Program::read(string fileName){
 */
 	}
 	source.close();
+
+	for(int i=0;i<tmp_program.size();i++){
+		for(int j=0;j<level_plus.size();j++){
+			if(tmp_program[i]==level_plus[j]){
+				try{
+					program.push_back(Statement(tmp_program[i],level,index_on_level[level]));
+					level++; index_on_level[level-1]++;
+				}
+				catch(std::out_of_range e){
+					index_on_level.push_back(1);
+					program.push_back(Statement(tmp_program[i],level,index_on_level[level]));
+					level++;
+				}
+				gotCoincidence=true;
+			}
+		}
+		for(int j=0;j<level_minus.size();j++){
+			if(tmp_program[i]==level_minus[j]){
+				level--; index_on_level[level]--;
+				program.push_back(Statement(tmp_program[i],level,index_on_level[level]));
+				gotCoincidence=true;
+			}
+		}
+		if(!gotCoincidence){
+			program.push_back(Statement(tmp_program[i],level,index_on_level[level]));
+			gotCoincidence=false;
+		}
+	}
 }
 
 int main(int argc, char *argv[]){
